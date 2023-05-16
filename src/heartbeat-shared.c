d4168a508fd866a9f08232072db60b1edafd4bad
Jurre Wolff <dev@jurrewolff.nl>
2023-05-16 14:46:41 +0200

    White space separating columns are now tab characters instead of spaces.
    Allows for more efficient parsing of the files by other programs.

--- src/heartbeat-shared.c
+++ src/heartbeat-shared.c
@@ -45,7 +45,7 @@ heartbeat_t* heartbeat_init(int64_t window_size,
       heartbeat_finish(hb);
       return NULL;
     } else {
-      fprintf(hb->text_file, "Beat    Tag    Timestamp    Global Rate    Window Rate    Instant Rate\n" );
+      fprintf(hb->text_file, "Beat\tTag\tTimestamp\tGlobal Rate\tWindow Rate\tInstant Rate\n" );
     }
   } else {
     hb->text_file = NULL;
@@ -109,7 +109,7 @@ static void hb_flush_buffer(heartbeat_t volatile * hb) {
   if(hb->text_file != NULL) {
     for(i = 0; i < nrecords; i++) {
       fprintf(hb->text_file,
-	      "%lld    %d    %lld    %f    %f    %f\n",
+	      "%lld\t%d\t%lld\t%f\t%f\t%f\n",
 	      (long long int) hb->log[i].beat,
 	      hb->log[i].tag,
 	      (long long int) hb->log[i].timestamp,
